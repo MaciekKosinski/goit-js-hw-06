80b46d9d0dcb5e4b47c70d3d58ba9871105cda07
Maciek Kosinski <maciek.kosinski87@gmail.com>
2022-09-17 13:45:42 +0200

    finish task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsElements = document.querySelector('#ingredients');
+
+console.log(ingredientsElements)
+
+const ingredientsList = ingredients.map((ingredient) => {
+  const ingredientsListTag = document.createElement('li');
+  ingredientsListTag.textContent = ingredient;
+  ingredientsListTag.classList.add("item");
+  return ingredientsListTag;
+});
+
+ingredientsElements.prepend(...ingredientsList);

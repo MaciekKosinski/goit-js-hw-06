62dfa1e9291fb9f5586c5942b64618a08b4019e6
Maciek Kosinski <maciek.kosinski87@gmail.com>
2022-09-17 13:12:47 +0200

    finish task 1

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const countCategories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${countCategories.length}`);
+
+countCategories.forEach((item) => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
+});

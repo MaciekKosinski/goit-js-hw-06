99325da5ecce76c5d3102a1b5a16edaa16774877
Maciek Kosinski <maciek.kosinski87@gmail.com>
2022-09-17 17:40:02 +0200

    finish task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,26 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galeryElements = document.querySelector('.gallery');
+
+console.log(galeryElements)
+
+const imagesElement = images
+  .map(function ({ url, alt }) {
+    return `<li><img src = ${url} alt = ${alt} width = '480'></li>`;
+  })
+  .join(" ");
+
+galeryElements.insertAdjacentHTML("afterbegin", imagesElement);
+  
+
+  
+galeryElements.style.cssText = `
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  list-style-type: none;
+  margin-top: 50px;
+  padding: 5px;
+`;

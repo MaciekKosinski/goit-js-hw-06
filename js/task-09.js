007c6ab1d9538f0d52ed30e49859c73f37f032b8
Maciek Kosinski <maciek.kosinski87@gmail.com>
2022-09-22 13:32:29 +0200

    finish task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const color = document.querySelector('.color');
+const changeColor = document.querySelector('.change-color');
+
+changeColor.addEventListener('mousedown', randomColor);
+
+function randomColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+};

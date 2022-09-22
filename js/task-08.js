cf9b6544968fa6f89efa18cf1878aea4330acec0
Maciek Kosinski <maciek.kosinski87@gmail.com>
2022-09-22 13:16:12 +0200

    finish task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", submitLogin);
+
+function submitLogin(event) {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget; 
+
+    if (email.value === "" || password.value === "") {
+        console.log("Wrong. Write email and password!");
+    } else {
+        console.log(`Email: ${email.value} and Password: ${password.value}`);
+        event.currentTarget.reset();
+    };
+};
